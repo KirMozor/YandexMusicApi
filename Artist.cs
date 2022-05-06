eda3327a3be022633e0f5e69b196572c071ff448
KirMozor <kirmozor@gmail.com>
2022-05-06 21:28:35 +0600

    Сделал новый класс Artist. Добавил туда метод для получения треков артиста

---
+++ Artist.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace YandexMusicApi
+{
+    public class Artist
+    {
+        private static string baseUrl = "https://api.music.yandex.net:443";
+
+        public static JObject GetTrack(string artistId, string page = "0", string pageSize = "20")
+        {
+            if (Token.token != "")
+            {
+                string urlToRequest = "/artists/" + artistId + "/tracks?page=" + page + "&page-size=" + pageSize;
+            
+                var header = new List<string>();
+            
+                header.Add("accept: */*");
+                header.Add("Authorization: OAuth " + Token.token);
+
+                string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
+                JObject adResponse =
+                    Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+                return adResponse;
+            }
+            else
+            {
+                string result = "{\"error\": \"Not token\"}";
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            }
+        }
+    }
+}
