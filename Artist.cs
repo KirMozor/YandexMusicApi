b97252dedf3f1bda915c3daded85b5f6447f4660
KirMozor <kirmozor@gmail.com>
2022-05-07 17:24:47 +0600

    Удалил бесполезные вызовы токена в классе Artist. Добавил новый метод для получения альбомов артиста

--- Artist.cs
+++ Artist.cs
@@ -9,69 +9,63 @@ namespace YandexMusicApi
 
         public static JObject GetTrack(string artistId, string page = "0", string pageSize = "20")
         {
-            if (Token.token != "")
-            {
-                string urlToRequest = "/artists/" + artistId + "/tracks?page=" + page + "&page-size=" + pageSize;
+            string urlToRequest = "/artists/" + artistId + "/tracks?page=" + page + "&page-size=" + pageSize;
             
-                var header = new List<string>();
+            var header = new List<string>();
             
-                header.Add("accept: */*");
-                header.Add("Authorization: OAuth " + Token.token);
-
-                string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
-                JObject adResponse =
-                    Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
-                return adResponse;
-            }
-            else
-            {
-                string result = "{\"error\": \"Not token\"}";
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
-            }
+            header.Add("accept: */*");
+                
+            string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
+            JObject adResponse =
+                Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            return adResponse;
         }
 
         public static JObject GetTrackIdByRating(string artistId)
         {
-            if (Token.token != "")
-            {
-                string urlToRequest = "/artists/" + artistId + "/track-ids-by-rating";
-                var header = new List<string>();
+            string urlToRequest = "/artists/" + artistId + "/track-ids-by-rating";
+            var header = new List<string>();
             
-                header.Add("accept: */*");
-                header.Add("Authorization: OAuth " + Token.token);
+            header.Add("accept: */*");
 
-                string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
-                JObject adResponse =
-                    Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
-                return adResponse;
-            }
-            else
-            {
-                string result = "{\"error\": \"Not token\"}";
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
-            }
+            string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
+            JObject adResponse =
+                Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            return adResponse;
         }
 
         public static JObject GetBriefInfo(string artistId)
         {
-            if (Token.token != "")
-            {
-                string urlToRequest = "/artists/" + artistId + "/brief-info";
-                var header = new List<string>();
+            string urlToRequest = "/artists/" + artistId + "/brief-info";
+            var header = new List<string>();
             
-                header.Add("accept: */*");
-                header.Add("Authorization: OAuth " + Token.token);
+            header.Add("accept: */*");
 
-                string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
-                JObject adResponse =
-                    Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
-                return adResponse;
+            string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
+            JObject adResponse =
+                Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            return adResponse;
+        }
+
+        public static JObject GetDirectAlbums(string artistId, string page = "0", string pageSize = "20", string sortBy = "--")
+        {
+            string urlToRequest;
+            if (sortBy == "--")
+            {
+                urlToRequest = "/artists/" + artistId + "/direct-albums?page=" + page + "&page-size=" + pageSize;
             }
             else
             {
-                string result = "{\"error\": \"Not token\"}";
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+                urlToRequest = "/artists/" + artistId + "/direct-albums?page=" + page + "&page-size=" + pageSize + "&sort-by=" + sortBy;
             }
+            var header = new List<string>();
+            
+            header.Add("accept: */*");
+
+            string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
+            JObject adResponse =
+                Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            return adResponse;
         }
     }
 }
