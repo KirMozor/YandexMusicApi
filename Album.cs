f6e2a620b6579ba9fbda935032953e7d219bdc81
KirMozor <kirmozor@gmail.com>
2022-05-08 01:44:25 +0600

    Сделал новый метод для получения информации о альбомах, отправляя массив List<string>

--- Album.cs
+++ Album.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
@@ -30,6 +31,34 @@ namespace YandexMusicApi
             header.Add("accept: application/json");
 
             string result = PostGet.GetWithHeaders(baseUrl + urlToRequest, header);
+            JObject adResponse =
+                Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            return adResponse;
+        }
+
+        public static JObject MoreInformAlbums(List<string> albumsId)
+        {
+            string urlToRequest = "/albums";
+            var header = new List<string>();
+            
+            header.Add("accept: application/json");
+            header.Add("Content-Type: application/x-www-form-urlencoded");
+
+            string albumsIdString = "";
+            int countAlbumsId = albumsId.Count;
+            
+            for (int i = 0; i < albumsId.Count; i++)
+            {
+                if (countAlbumsId - 1 == i)
+                {
+                    albumsIdString += albumsId[i];
+                }
+                else
+                {
+                    albumsIdString += albumsId[i] + ",";
+                }
+            }
+            string result = PostGet.PostDataAndHeaders(baseUrl + urlToRequest, "album-ids=" + albumsIdString, header);
             JObject adResponse =
                 Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
             return adResponse;
