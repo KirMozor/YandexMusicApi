fe222e6ed29c4937cbe1e70a79719991113c332e
KirMozor <kirmozor@gmail.com>
2022-05-08 20:59:39 +0600

    Сделал новый метод для удаленияпонравившихся треков

--- Track.cs
+++ Track.cs
@@ -48,5 +48,47 @@ namespace YandexMusicApi
                 return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
             }
         }
+
+        public static JObject RemoveLikesTracks(List<string> likeTracks, string userId)
+        {
+            if (Token.token != "")
+            {
+                string urlToRequest = "/users/" + userId + "/likes/tracks/remove";
+
+                var header = new List<string>();
+
+                header.Add("accept: application/json");
+                header.Add("Content-Type: application/x-www-form-urlencoded");
+                header.Add("Authorization: OAuth " + Token.token);
+
+                string removeTracksIdString = "";
+                int countTracksId = likeTracks.Count;
+
+                for (int i = 0; i < likeTracks.Count; i++)
+                {
+                    if (countTracksId - 1 == i)
+                    {
+                        removeTracksIdString += likeTracks[i];
+                    }
+                    else
+                    {
+                        removeTracksIdString += likeTracks[i] + ",";
+                    }
+                }
+                
+                string dataRequest = "track-ids=" + removeTracksIdString;
+            
+                string result = PostGet.PostDataAndHeaders(baseUrl + urlToRequest, dataRequest, header);
+                JObject adResponse =
+                    Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+                return adResponse;
+            }
+
+            else
+            {
+                string result = "{\"error\": \"Not token\"}";
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(result);
+            }
+        }
     }
 }
